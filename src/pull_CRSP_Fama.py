53086a3a1c25e4933d3a1e78d5bcd57594e7a569
GRAN1619 <gran@uchicago.edu>
2025-02-25 10:31:29 -0600

    adding in col descriptions

--- src/pull_CRSP_Fama.py
+++ src/pull_CRSP_Fama.py
@@ -1,6 +1,12 @@
 import wrds
 import pandas as pd
 
+#See here: https://www.crsp.org/wp-content/uploads/guides/CRSP_US_Treasury_Database_Guide_for_SAS_ASCII_EXCEL_R.pdf
+description_crsp_fama = {
+    "mcaldt: Last Quotation Date in the Month",
+    "kytreasnox: Treasury Record Identifier", 
+    "tmewretd: Monthly Equal Weighted Portfolio Return"
+}
 def pull_fama_bond_portfolios(start_date='1960-01-31', end_date='2024-12-31'):
     db = wrds.Connection()
 
